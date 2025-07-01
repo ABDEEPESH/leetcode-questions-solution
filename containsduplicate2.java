033204716644804ba8f715e69b4dcff19e29c5c3
ABDEEPESH <155074929+ABDEEPESH@users.noreply.github.com>
2025-07-01 13:01:53 +0530

    Create containsduplicate2.java

---
+++ containsduplicate2.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j <= Math.min(i + k, nums.length - 1); j++) {
+                if (nums[i] == nums[j]) {
+                    return true; // Duplicate found within window
+                }
+            }
+        }
+        return false; // No duplicates within window
+    }
+}
